a21b6acd5c721cb2e717ae9d3bb1558d187292cc
Luca Ferrari <fluca1978@gmail.com>
2010-07-28 17:54:17 +0200

    Created a simple skeleton test for the function builder.

---
+++ srcTest/jfk/test/FunctionTest.java
@@ -0,0 +1,57 @@
+/* 
+ * JFK - Java Function Kernel
+ *
+ * This project provides a run-time framework to achieve functional programming
+ * in Java. The idea is to have the capability to get something similar to function pointers
+ * and C# delegates in Java, doing all the bindings and reference resolution at run-time being able,
+ * at the same time, being able to compile the program using a function-first-class entity and abstraction.
+ *
+ * Copyright (C) Luca Ferrari 2010-2011 - cat4hire@users.sourceforge.net
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package jfk.test;
+
+
+import jfk.core.JFK;
+import jfk.function.IFunctionBuilder;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Luca Ferrari - cat4hire (at) users.sourceforge.net
+ *
+ */
+public class FunctionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testFunctionBuilder(){
+	// get a new function builder
+	IFunctionBuilder fBuilder = JFK.getFunctionBuilder();
+	
+	// should always have a function builder
+	if( fBuilder == null )
+	    fail("Did not get a function builder from the configuration!");
+    }
+
+
+}

f22b46b5fee088c599b53a9c400d2528ee053103
Luca Ferrari <fluca1978@gmail.com>
2010-08-02 09:09:52 +0200

    Create the base interfaces and exception for the delegates.

---
+++ src/jfk/function/exception/delegates/AlreadyImplementedDelegateException.java
@@ -0,0 +1,68 @@
+/* 
+ * JFK - Java Function Kernel
+ *
+ * This project provides a run-time framework to achieve functional programming
+ * in Java. The idea is to have the capability to get something similar to function pointers
+ * and C# delegates in Java, doing all the bindings and reference resolution at run-time being able,
+ * at the same time, being able to compile the program using a function-first-class entity and abstraction.
+ *
+ * Copyright (C) Luca Ferrari 2010-2011 - cat4hire@users.sourceforge.net
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package jfk.function.exception.delegates;
+
+/**
+ * An exception to indicate that the user is trying to attach an already implemented method to
+ * a delegate. The rule is that a delegate method must be abstract, because its implementation must be obtained
+ * at run-time.
+ * @author Luca Ferrari - cat4hire (at) users.sourceforge.net
+ *
+ */
+public class AlreadyImplementedDelegateException extends DelegateException {
+
+    /**
+     * 
+     */
+    public AlreadyImplementedDelegateException() {
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public AlreadyImplementedDelegateException(String message, Throwable cause) {
+	super(message, cause);
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public AlreadyImplementedDelegateException(String message) {
+	super(message);
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public AlreadyImplementedDelegateException(Throwable cause) {
+	super(cause);
+	// TODO Auto-generated constructor stub
+    }
+
+}

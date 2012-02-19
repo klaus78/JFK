3e3fdcfbd55137f7060dbc18a3dc546eb96bba6e
klaus78 <tagliole@libero.it>
2012-02-19 14:21:07 +0100

    first files for role

---
+++ src/main/java/jfk/role/IRole.java
@@ -0,0 +1,5 @@
+package jfk.role;
+
+public interface IRole {
+	public String getRoleName();
+}
---
+++ src/main/java/jfk/role/IRoleManager.java
@@ -0,0 +1,14 @@
+package jfk.role;
+
+public interface IRoleManager {
+	/**
+     * Add a role to the target class.
+     * @param target the class receiving the new role
+     * @param role the role to be added to the target
+     * @return void 
+     */
+	public void addRole(Class target, Class role);
+	
+	// public bool addRole(Class target, Interface irole)
+    public IRole getAsRole(Class target, IRole role);
+}
